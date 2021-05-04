15a652824bb8761fb540daf8dd5bbcafe0ee4104
indigostarrgit config --global user.name 'indigostarr <indigojstarr@gmail.com>
2021-05-04 11:45:30 -0500

    refactoring and adding notation

--- script.js
+++ script.js
@@ -1,54 +1,67 @@
-// create console game to later be modified into
+// set scores
+let playerScoreNum = 0;
+let computerScoreNum = 0;
 
-// create a function for the computer
-// randomly select between rock, paper, scissors
+// declare variables for player selection
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
+
+// declare variables for
 const message = document.querySelector(".message");
-const body = document.querySelector("body");
+
+// declare variables for computer and player scoreboard
 const playerScoreBoard = document.querySelectorAll("#player-score-board");
 const computerScoreBoard = document.querySelectorAll("#computer-score-board");
 let playerScore = document.querySelector("#player-score");
 let computerScore = document.querySelector("#computer-score");
+
+// declare variables for computer and player choices to modify opacity
 let playerChoices = document.getElementsByClassName("player-choices");
 let computerChoices = document.getElementsByClassName("computer-choices");
+
+// button to start a new game of 5 rounds
 let newGame = document.querySelector("button");
 
+// function that resets opacity after each round
 function showChoices(choices) {
   Array.from(choices).forEach((element) => {
     element.classList.remove("not-selected");
   });
 }
 
-function resetMatch() {
-  showChoices(playerScoreBoard);
-  showChoices(computerScoreBoard);
-  showChoices(playerChoices);
-  showChoices(computerChoices);
-}
-
+// function that sets opacity to 0.5 once each round completes
 function notSelected(choices) {
   Array.from(choices).forEach((element) => {
     element.classList.add("not-selected");
   });
 }
 
+// function to hide the modal message
 function hideMessageModal() {
   message.classList.add("hidden");
 }
+setTimeout(hideMessageModal, 2000);
 
+// function that resets opacity of the score board and anything not selected during end round screen
+function resetRound() {
+  setTimeout(() => message.classList.remove("hidden"), 500);
+  setTimeout(() => notSelected(playerScoreBoard), 500);
+  setTimeout(() => notSelected(computerScoreBoard), 500);
+  setTimeout(hideMessageModal, 2000);
+  setTimeout(() => showChoices(playerScoreBoard), 2000);
+  setTimeout(() => showChoices(computerScoreBoard), 2000);
+  setTimeout(() => showChoices(playerChoices), 2000);
+  setTimeout(() => showChoices(computerChoices), 2000);
+}
+
+// function to end the game after 5 rounds
 function endGame() {
   message.classList.remove("hidden");
-  notSelected(playerScoreBoard);
-  notSelected(computerScoreBoard);
   paper.removeEventListener("click", game);
   rock.removeEventListener("click", game);
   scissors.removeEventListener("click", game);
 }
-
-let playerScoreNum = 0;
-let computerScoreNum = 0;
 
 paper.addEventListener("click", game);
 rock.addEventListener("click", game);
@@ -57,7 +70,9 @@ scissors.addEventListener("click", game);
 newGame.addEventListener("click", () => {
   message.textContent = "Ready to Play!";
   message.classList.remove("hidden");
+  playerScoreNum = 0;
   playerScore.textContent = 0;
+  computerScoreNum = 0;
   computerScore.textContent = 0;
   showChoices(playerChoices, computerChoices);
   paper.addEventListener("click", game);
@@ -66,40 +81,27 @@ newGame.addEventListener("click", () => {
 });
 
 function game(event) {
-  hideMessageModal();
+  // hideMessageModal();
 
   let playerSelection = event.currentTarget.alt;
 
+  // set opacity for choices not selected
   for (let i = 0; i < playerChoices.length; i++) {
     if (playerSelection != playerChoices[i].alt) {
       playerChoices[i].classList.add("not-selected");
     }
   }
 
+  // create variable for computer choice
   let computerSelection = computerPlay();
+  // use variable to set the img for computer
   document.getElementById("question").src = `${computerSelection}.png`;
+  // use player and computer variables to play the game
   rockPaperScissors(computerSelection, playerSelection);
   console.log(playerScoreNum, computerScoreNum);
-  // body.font = light grey
-  // add selected to player and computer
-  // remove hidden on delay to a pop up
-
-  // reset screen
-
-  if (
-    (playerScoreNum === 5 && computerScoreNum < 5) ||
-    (computerScoreNum === 5 && playerScoreNum < 5)
-  ) {
-    endGame();
-  } else {
-    setTimeout(() => message.classList.remove("hidden"), 700);
-    setTimeout(() => notSelected(playerScoreBoard), 750);
-    setTimeout(() => notSelected(computerScoreBoard), 750);
-    setTimeout(hideMessageModal, 3000);
-    setTimeout(resetMatch, 3000);
-  }
 }
 
+// create a function for the computer's to randomly select between rock, paper, scissors
 function computerPlay() {
   // use Math.random to generate 1-3
   let play = Math.floor(Math.random() * 3);
@@ -120,28 +122,42 @@ function computerPlay() {
 
 // create a game to take computer and player inputs and declare a winner
 function rockPaperScissors(computerSelection, playerSelection) {
-  if (computerSelection === playerSelection) {
-    message.textContent = `Tie you both selected ${computerSelection}`;
+  if (
+    (computerSelection === "Paper" && playerSelection === "Rock") ||
+    (computerSelection === "Scissors" && playerSelection === "Paper") ||
+    (computerSelection === "Rock" &&
+      playerSelection === "Scissors" &&
+      (playerScoreNum <= 5 || computerScoreNum <= 5))
+  ) {
+    computerScoreNum++;
+    computerScore.textContent = computerScoreNum;
+    if (computerScoreNum === 5) {
+      message.textContent = `Computer wins the game with 5 points!`;
+      notSelected(playerScoreBoard);
+      endGame();
+    } else {
+      message.textContent = `Computer wins! ${computerSelection} beats ${playerSelection}`;
+      resetRound();
+    }
   } else if (
-    (computerSelection === "Rock" && playerSelection === "Paper") ||
-    (computerSelection === "Paper" && playerSelection === "Scissors")
+    (playerSelection === "Paper" && computerSelection === "Rock") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper") ||
+    (playerSelection === "Rock" &&
+      computerSelection === "Scissors" &&
+      (playerScoreNum <= 5 || computerScoreNum <= 5))
   ) {
     playerScoreNum++;
     playerScore.textContent = playerScoreNum;
     if (playerScoreNum === 5) {
       message.textContent = `You win the game with 5 points!`;
+      notSelected(computerScoreBoard);
       endGame();
     } else {
       message.textContent = `You win ${playerSelection} beats ${computerSelection}!`;
+      resetRound();
     }
   } else {
-    computerScoreNum++;
-    computerScore.textContent = computerScoreNum;
-    if (computerScoreNum === 5) {
-      message.textContent = `Computer wins the game with 5 points!`;
-      endGame();
-    } else {
-      message.textContent = `Computer wins! ${computerSelection} beats ${playerSelection}`;
-    }
+    message.textContent = `Tie you both selected ${computerSelection}`;
+    resetRound();
   }
 }
